1f474d89dc293f804786505d2ca7791f1a797960
Michael D. Krämer <mkraemer@diw.de>
2023-07-13 18:06:57 +0200

    setting up analysis .qmd

--- 00_singlehood_mainscript.R
+++ 00_singlehood_mainscript.R
@@ -1,13 +1,17 @@
 # Main script
 
+library(renv)
+library(rmarkdown)
+library(quarto)
+
 ### Package / dependency version management with 'renv'
 # only ran this to initialize 'renv'
 # renv::init()
 # renv::snapshot()
 # renv::restore() # to revert to the previous state as encoded in the lockfile
 
-### generate data from SHARE data files 
-source("01_datacleaning.Rmd")
+### generate data from SHARE data files (created by AS)
+rmarkdown::render("01_datacleaning.Rmd")
 
 ### run analysis scripts
-# source("02_analyses.qmd")
+# quarto_render("02_analyses.qmd")
